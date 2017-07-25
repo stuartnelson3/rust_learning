b3908d6dc0198f05500075abc0476552fbda2aa3
Stuart Nelson <stuartnelson3@gmail.com>
2017-07-25 23:57:37 +0200

    Remove some comments and printline statements

--- src/main.rs
+++ src/main.rs
@@ -17,12 +17,9 @@ fn jaro(s1: &str, s2: &str) -> f64 {
 
     let z2 = s2.char_indices();
 
-    // Not sure if I can just drop l1 and l2 into a slice like that.
-    // Might not have to subtract the 1 if we're using 0 indexed string.
     let sl = [l1, l2];
     // Integer division, not necessary to floor result.
     let match_range = (sl.iter().max().unwrap() / 2) - 1;
-    print!("match_range {}\n", match_range);
 
     let m: Vec<(usize, char)> = s1.char_indices()
         .flat_map(|idx_char| char_match(match_range, z2.clone(), idx_char))
@@ -30,7 +27,6 @@ fn jaro(s1: &str, s2: &str) -> f64 {
     let ml = m.len() as f64;
     let t: f64 = m.iter().map(|&v| transposition(z2.clone(), v) * 0.5).sum();
 
-    print!("char_match {:?}\n", m);
     let ml1 = ml / (l1 as f64);
     let ml2 = ml / (l2 as f64);
     let mtm = (ml - t) / ml;
