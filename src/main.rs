e2f83ba41ff15f3fd8c28d8f1888f29ea64f0648
stuart nelson <stuartnelson3@gmail.com>
2017-09-20 20:53:55 +0200

    command line flags

--- src/main.rs
+++ src/main.rs
@@ -25,22 +25,23 @@ fn jaro(s1: &str, s2: &str) -> f64 {
         .flat_map(|idx_char| char_match(match_range, z2.clone(), idx_char))
         .collect();
     let ml = m.len() as f64;
+
+    if ml == 0.0 {
+        return 0.0;
+    }
+
     let t: f64 = m.iter().map(|&v| transposition(z2.clone(), v) * 0.5).sum();
 
     let ml1 = ml / (l1 as f64);
     let ml2 = ml / (l2 as f64);
     let mtm = (ml - t) / ml;
 
-    match ml {
-        0.0 => 0.0,
-        _ => 0.33 * (ml1 + ml2 + mtm),
-    }
+    0.33 * (ml1 + ml2 + mtm)
 }
 
 fn transposition(list: CharIndices, pair: (usize, char)) -> f64 {
     let (p, q) = pair;
-    let res: Vec<(usize, char)> = list.filter(|idx_char| p != idx_char.0 && q == idx_char.1)
-        .collect();
+    let res: Vec<(usize, char)> = list.filter(|&(idx, chr)| p != idx && q == chr).collect();
     res.len() as f64
 }
 
@@ -53,8 +54,40 @@ fn char_match(match_range: usize, list: CharIndices, pair: (usize, char)) -> Vec
         .collect()
 }
 
+extern crate clap;
+
+use clap::{App, Arg};
+
 fn main() {
-    let res = jaro("bizmarkie", "zibawekj");
+    let matches = App::new("jaro string comparison")
+        .version("0.1.0")
+        .author("stuart nelson <stuartnelson3@gmail.com>")
+        .about("compares too strings for similarity")
+        .arg(
+            Arg::with_name("s1")
+                .long("source")
+                .value_name("string1")
+                .help("the first string")
+                .required(true)
+                .takes_value(true),
+        )
+        .arg(
+            Arg::with_name("s2")
+                .long("compare")
+                .help("the second string")
+                .required(true)
+                .takes_value(true),
+        )
+        .get_matches();
+
+    let s1 = matches.value_of("s1").unwrap();
+    println!("Value for s1: {}", s1);
+
+    let s2 = matches.value_of("s2").unwrap();
+    println!("Value for s2: {}", s2);
+
+    // let res = jaro("bizmarkie", "zibawekj");
+    let res = jaro(s1, s2);
     print!("result: {}", res);
     // let listener = TcpListener::bind("0.0.0.0:5000").unwrap();
     //
